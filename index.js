b1ffa7fcc7a27a67f6dab4be498bb853d421a968
joyadeep <joyadeeplimbu@gmail.com>
2022-12-07 15:50:42 +0545

    added dotenv variables

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 import express from 'express'
+import dotenv from 'dotenv'
 const app=express();
+dotenv.config();
+
+const port=process.env.PORT||3000
 
 app.get("/",(req,res)=>{
     res.status(200).json({success:true,message:'hello server'})
 })
 
-app.listen(5000,()=>{
-    console.log("server running at port 5000")
+app.listen(port,()=>{
+    console.log("server running at port ",port)
 })
