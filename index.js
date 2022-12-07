dff3628029e8fb0f67474d247003f074e4dd4326
joyadeep <joyadeeplimbu@gmail.com>
2022-12-07 15:33:05 +0545

    modified index.js

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 import express from 'express'
 const app=express();
 
+app.get("/",(req,res)=>{
+    res.status(200).json({success:true,message:'hello server'})
+})
+
 app.listen(5000,()=>{
     console.log("server running at port 5000")
 })
